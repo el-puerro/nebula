30b83be5e895846d14e71f5aeee787e835e6e797
Mielke <mielke@N-00442.YLD.LAN>
2022-12-20 09:59:46 +0100

    it finally fucking compiles

--- sources/kernel/include/mem.h
+++ sources/kernel/include/mem.h
@@ -6,13 +6,13 @@
 // This is actually a mmap entry, not the entire map
 typedef multiboot_memory_map_t mmap_entry_t;
 
-//multiboot_info_t* multiboot_info;
+extern multiboot_info_t* multiboot_info;
 
-//int pmem_index_free;
-//int pmem_index_reserved;
+extern int pmem_index_free;
+extern int pmem_index_reserved;
 
-//mmap_entry_t* pmem_list_free[20];
-//mmap_entry_t* pmem_list_reserved[20];
+extern mmap_entry_t* pmem_list_free[20];
+extern mmap_entry_t* pmem_list_reserved[20];
 
 void get_memmap(multiboot_info_t* mbd, uint32_t magic);
 void memmap_init_lists();
